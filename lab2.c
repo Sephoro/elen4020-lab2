5ba57933ea68494dd9bfb8309f341ad6573cc8a4
Boikanyo13 <bv.radiokana@gmail.com>
2019-03-14 22:05:57 +0200

    A Naive-Threading Algorithm-OpenMP

--- lab2.c
+++ lab2.c
@@ -1,124 +1,128 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <omp.h>
 
-int SIZE=0;
+int SIZE = 0;
 
 //generate random nxn matrix
-void fill2D(int twoD[SIZE][SIZE]){
-   
-   int num = 0;
+void fill2D(int twoD[SIZE][SIZE])
+{
 
-   for(int i =0; i < SIZE; i++){
-   
-      for(int j =0; j < SIZE; j++){
-          
-          num = rand()%20; 
-          twoD[i][j] = num; 
-      
-         }
-   
-     }
+	int num = 0;
 
-  }
+	for (int i = 0; i < SIZE; i++)
+	{
 
+		for (int j = 0; j < SIZE; j++)
+		{
+
+			num = rand() % 20;
+			twoD[i][j] = num;
+		}
+	}
+}
 
 //display matrix
-void print(int twoD[SIZE][SIZE]){
+void print(int twoD[SIZE][SIZE])
+{
 
-    for(int i = 0; i <SIZE; i++){
-    
-	    for(int j = 0; j <SIZE; j++){
-	    	
-		    printf("%i ", twoD[i][j]);
-	    
-	    }
-	
-	    printf("\n");
-    
-    }
+	for (int i = 0; i < SIZE; i++)
+	{
 
-    printf("\n");
-}
+		for (int j = 0; j < SIZE; j++)
+		{
 
+			printf("%i ", twoD[i][j]);
+		}
 
-void basicTranspose(int twoD[SIZE][SIZE]){
-
- int temp =0;
-
- for(int i=0; i < SIZE; i++){
- 
- 	for(int j =0; j < i; j++){
-	       
-	        temp = twoD[i][j];
-		twoD[i][j] = twoD[j][i];
-		twoD[j][i] =temp;
-	 
+		printf("\n");
 	}
-   }
 
+	printf("\n");
 }
 
-void diagonalTranspose(int twoD[SIZE][SIZE]){
+//A Naive-Threaded algorithm
+void basicTranspose(int twoD[SIZE][SIZE])
+{
 
-int temp =0;
+	int temp, i, j = 0;
+	omp_set_num_threads(8);
 
-for(int i=0; i < SIZE; i++){
- 
- 	for(int j =0; j < i+1; j++){
-	       
-    
-    if(i==j){
-    
-       //printf("%i and %i ",i,j);
-	  //  printf("\n\n");
-	    
-	    for(int a =i; a < SIZE; a++){
-	          
-	          temp = twoD[i][a];
-	          twoD[i][a] = twoD[a][i];
-		        twoD[a][i] = temp;
-	 
-	          
-	    }
-	    
-    }  
-	 }
- }
+#pragma omp parallel for lastprivate(i)
+	for (int i = 0; i < SIZE; i++)
+	{
+
+		for (int j = 0; j < i; j++)
+		{
+
+			temp = twoD[i][j];
+			twoD[i][j] = twoD[j][i];
+			twoD[j][i] = temp;
+			
+		}
+	}
 }
 
+void diagonalTranspose(int twoD[SIZE][SIZE])
+{
 
-int main(){
- 
- //generate seed
- srand(time(0));
+	int temp = 0;
 
- printf("Please input size: ");
- scanf("%i", &SIZE);
+	for (int i = 0; i < SIZE; i++)
+	{
 
- int (*twoD)[SIZE] = malloc(sizeof(int[SIZE][SIZE])); //Two Dimensional Array
- int (*mat)[SIZE] = malloc(sizeof(int[SIZE][SIZE])); //Two Dimensional Array
- 
+		for (int j = 0; j < i + 1; j++)
+		{
 
- printf("You entered %i as the size of the 2D matrix\n\n", SIZE);
+			if (i == j)
+			{
 
- fill2D(twoD);
- print(twoD);
+				//printf("%i and %i ",i,j);
+				//  printf("\n\n");
 
- printf("Now Transposing...\n\n");
+				for (int a = i; a < SIZE; a++)
+				{
 
- basicTranspose(twoD);
- print(twoD);
- 
- printf("\n\n\n\n");
- fill2D(mat);
- print(mat);
- 
- printf("Now Transposing AGAIN...\n\n");
- 
- diagonalTranspose(mat);
-  print(mat);
+					temp = twoD[i][a];
+					twoD[i][a] = twoD[a][i];
+					twoD[a][i] = temp;
+				}
+			}
+		}
+	}
+}
 
- return 0;
- }
+int main()
+{
 
+	//generate seed
+	srand(time(0));
+
+	printf("Please input size: ");
+	scanf("%i", &SIZE);
+
+	int(*twoD)[SIZE] = malloc(sizeof(int[SIZE][SIZE])); //Two Dimensional Array
+	int(*mat)[SIZE] = malloc(sizeof(int[SIZE][SIZE]));	//Two Dimensional Array
+
+	printf("You entered %i as the size of the 2D matrix\n\n", SIZE);
+
+	fill2D(twoD);
+	print(twoD);
+
+	printf("Now Transposing...\n\n");
+
+	basicTranspose(twoD);
+	print(twoD);
+
+	//printf("\n\n\n\n");
+	//fill2D(mat);
+	//print(mat);
+
+	//printf("Now Transposing AGAIN...\n\n");
+
+	//diagonalTranspose(mat);
+	// print(mat);
+
+	return 0;
+}

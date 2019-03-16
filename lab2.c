fd37928ae9594c0c4a80bba9d6e1ecacea809bc4
Boikanyo13 <bv.radiokana@gmail.com>
2019-03-16 12:37:59 +0200

    Daigonal Threading-OpenMP

--- lab2.c
+++ lab2.c
@@ -42,14 +42,12 @@ void print(int twoD[SIZE][SIZE])
 	printf("\n");
 }
 
-//A Naive-Threaded algorithm
-void basicTranspose(int twoD[SIZE][SIZE])
+// Basic Naive Transpose
+void NaiveTransposeBasic(int twoD[SIZE][SIZE])
 {
 
-	int temp, i, j = 0;
-	omp_set_num_threads(8);
+	int temp = 0;
 
-#pragma omp parallel for lastprivate(i)
 	for (int i = 0; i < SIZE; i++)
 	{
 
@@ -59,39 +57,70 @@ void basicTranspose(int twoD[SIZE][SIZE])
 			temp = twoD[i][j];
 			twoD[i][j] = twoD[j][i];
 			twoD[j][i] = temp;
-			
 		}
 	}
 }
 
-void diagonalTranspose(int twoD[SIZE][SIZE])
+// Basic Naive Transpose
+void NaiveTransposeOpenMP(int twoD[SIZE][SIZE])
+{
+
+	int temp = 0;
+	omp_set_num_threads(8);
+
+    #pragma omp parallel for
+	for (int i = 0; i < SIZE; i++)
+	{
+
+		for (int j = 0; j < i; j++)
+		{
+
+			temp = twoD[i][j];
+			twoD[i][j] = twoD[j][i];
+			twoD[j][i] = temp;
+		}
+	}
+}
+
+//Basic Diagonal Transpose
+void DiagonalTransposeBasic(int twoD[SIZE][SIZE])
 {
 
 	int temp = 0;
 
 	for (int i = 0; i < SIZE; i++)
 	{
-
-		for (int j = 0; j < i + 1; j++)
+		for (int j = i + 1; j < SIZE; j++)
 		{
 
-			if (i == j)
-			{
-
-				//printf("%i and %i ",i,j);
-				//  printf("\n\n");
-
-				for (int a = i; a < SIZE; a++)
-				{
-
-					temp = twoD[i][a];
-					twoD[i][a] = twoD[a][i];
-					twoD[a][i] = temp;
-				}
-			}
+			temp = twoD[i][j];
+			twoD[i][j] = twoD[j][i];
+			twoD[j][i] = temp;
 		}
 	}
 }
+
+//OpenMP Diagonal Transpose
+void DiagonalTransposeOpenMP(int twoD[SIZE][SIZE])
+{
+
+	int temp = 0;
+	omp_set_num_threads(8);
+
+    #pragma omp parallel for
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = i + 1; j < SIZE; j++)
+		{
+
+			temp = twoD[i][j];
+			twoD[i][j] = twoD[j][i];
+			twoD[j][i] = temp;
+		}
+	}
+}
+
+
 
 int main()
 {
@@ -103,26 +132,32 @@ int main()
 	scanf("%i", &SIZE);
 
 	int(*twoD)[SIZE] = malloc(sizeof(int[SIZE][SIZE])); //Two Dimensional Array
-	int(*mat)[SIZE] = malloc(sizeof(int[SIZE][SIZE]));	//Two Dimensional Array
-
+	
 	printf("You entered %i as the size of the 2D matrix\n\n", SIZE);
 
 	fill2D(twoD);
 	print(twoD);
 
-	printf("Now Transposing...\n\n");
-
-	basicTranspose(twoD);
+	printf("Naive basic...\n");
+	NaiveTransposeBasic(twoD);
 	print(twoD);
 
-	//printf("\n\n\n\n");
-	//fill2D(mat);
-	//print(mat);
+	printf("\n");
+	
+	printf("Naive OPM...\n");
+	NaiveTransposeOpenMP(twoD);
+	print(twoD);
 
-	//printf("Now Transposing AGAIN...\n\n");
+	printf("\n");
 
-	//diagonalTranspose(mat);
-	// print(mat);
+	printf("Diagonal basic...\n");
+	DiagonalTransposeBasic(twoD);
+	print(twoD);
+
+    printf("\n");
+    printf("Diagonal OPM...\n");
+	DiagonalTransposeOpenMP(twoD);
+	print(twoD);
 
 	return 0;
 }

533976f2ea64b1fd950cb69c09beba529c8d836e
sephoro <eliassepuru@gmail.com>
2019-03-20 04:45:18 +0200

    Pthread Block Transposition

--- lab2.c
+++ lab2.c
@@ -6,10 +6,10 @@
 #include <string.h>
 #include <pthread.h>
 
-int SIZE = 16;
-int BLOCK_SIZE = 4;
-int nextPosition = 0;
-int numOfThreads = 8;
+int SIZE = 128;
+int BLOCK_SIZE = 16;
+int NEXT_POS = 0;
+int NUM_OF_THREADS = 8;
 
 pthread_mutex_t syncing;
 
@@ -50,6 +50,7 @@ void print(int twoD[SIZE][SIZE])
 	printf("\n");
 }
 
+
 void *swap(int *a, int *b){
 
    int temp = *a;
@@ -77,7 +78,7 @@ void NaiveTransposeOpenMP(int twoD[SIZE][SIZE])
 {
 
 
-omp_set_num_threads(numOfThreads);
+omp_set_num_threads(NUM_OF_THREADS);
 
     #pragma omp parallel 
     {
@@ -112,7 +113,7 @@ void DiagonalTransposeOpenMP(int twoD[SIZE][SIZE])
 {
 
 	int temp = 0;
-	omp_set_num_threads(numOfThreads);
+	omp_set_num_threads(NUM_OF_THREADS);
 
     #pragma omp parallel
 	{
@@ -147,46 +148,46 @@ void* pThread_diagonalTranspose(void* thread)
     
     struct threadInfo *tInfo = (struct threadInfo *) thread;
     int SIZE = tInfo->size;
-    
 
-   while(1)
-   { 
-             int temp;
-				    for (int j = tInfo->pos+1; j < SIZE; ++j)
-	          {
+		while (1)
+		{
+			int temp;
+			for (int j = tInfo->pos + 1; j < SIZE; ++j)
+			{
 
-					  temp =  *(tInfo->Arr+ SIZE*tInfo->pos +j);
-			      *(tInfo->Arr+ SIZE*tInfo->pos +j) = *(tInfo->Arr+ SIZE*j +tInfo->pos);
-					  *(tInfo->Arr+ SIZE*j +tInfo->pos) = temp;
-				
-	          }
-					
-			       pthread_mutex_lock(&syncing);
-      
-            if(nextPosition < SIZE-1)
-             {
-            
-                tInfo->pos = nextPosition;
-                ++nextPosition;
-            }else tInfo->pos = SIZE-1;
-            pthread_mutex_unlock(&syncing);
-            
-            if (tInfo->pos==SIZE-1) break;
- 
-    }
-      pthread_exit(NULL);
+				temp = *(tInfo->Arr + SIZE * tInfo->pos + j);
+				*(tInfo->Arr + SIZE * tInfo->pos + j) = *(tInfo->Arr + SIZE * j + tInfo->pos);
+				*(tInfo->Arr + SIZE * j + tInfo->pos) = temp;
+			}
+
+			pthread_mutex_lock(&syncing);
+
+			if (NEXT_POS < SIZE - 1)
+			{
+
+				tInfo->pos = NEXT_POS;
+				++NEXT_POS;
+			}
+			else
+				tInfo->pos = SIZE - 1;
+			pthread_mutex_unlock(&syncing);
+
+			if (tInfo->pos == SIZE - 1)
+				break;
+		}
+		pthread_exit(NULL);
 }
 
 
 void DiagonalTransposePthread(int twoD[SIZE][SIZE])
 {
 
-	pthread_t threads[numOfThreads];
-	struct threadInfo threadParameters[numOfThreads];
-	nextPosition = numOfThreads;
+	pthread_t threads[NUM_OF_THREADS];
+	struct threadInfo threadParameters[NUM_OF_THREADS];
+	NEXT_POS = NUM_OF_THREADS;
 	
 	
-	for(int i = 0; i < numOfThreads; i++)
+	for(int i = 0; i < NUM_OF_THREADS; i++)
 	{
 		threadParameters[i].size = SIZE;
 		threadParameters[i].pos = i;
@@ -196,7 +197,7 @@ void DiagonalTransposePthread(int twoD[SIZE][SIZE])
 		pthread_create (&threads[i], NULL, &pThread_diagonalTranspose,&threadParameters[i]);	
 	}
 
-  	for(int i = 0; i < numOfThreads; i++)
+  	for(int i = 0; i < NUM_OF_THREADS; i++)
 	{
 		pthread_join (threads[i], NULL);	
 	}
@@ -296,6 +297,169 @@ void BlockTransposeBasic(int twoD[SIZE][SIZE]){
  }
 
 
+/** PTHREADS BLOCK TRANSPOSITION AHEAD **/
+
+//Below are the pthread Block Transposition variables
+
+struct blockThreadInfo{
+
+   int row;
+	 int *arr;
+
+};
+
+int NEXT_ROW_E = 0;
+int NEXT_ROW_B = 0;
+pthread_mutex_t stillBusy; 
+
+// pthread version of BlockElement Transpose
+
+void* PthreadsBlockElementTranspose( void* blockInfo){
+
+
+  struct blockThreadInfo *blockThread = (struct blockThreadInfo*) blockInfo;
+   
+  while(1){
+
+   	int otherRow = 0;
+   	int otherCol = 0;
+
+   	for(int i = blockThread->row; i < (blockThread->row+BLOCK_SIZE); i+=BLOCK_SIZE){
+   
+     		for(int j=0; j < SIZE ; j+=BLOCK_SIZE){
+     
+        		for(int k = 0; k < BLOCK_SIZE; k++){
+	  
+	       			for(int l = 0; l < k; l++){
+	          
+                  		 	if(i!=j){
+                               
+	                               	         //Not on diagonal
+                      		     	         otherRow =  (i + l);
+                          		  	 otherCol =  (j + k);                       
+                    					}
+                    			else{
+                             			//Diagonal
+                        		 	otherRow = (l + j);
+                           			otherCol = (k + i);
+                    					}
+ 
+                     			swap((blockThread->arr+ (k+i)*SIZE + (l+j)), (blockThread->arr + (otherRow)*SIZE + (otherCol)));
+              				     
+				 }
+			 }
+      		   }
+	   }
+
+ 
+
+    //Done?
+   if(NEXT_ROW_E >= SIZE - 1) break;
+     
+    //Controlled Entrance
+
+     pthread_mutex_lock(&stillBusy);
+    
+     		blockThread->row = NEXT_ROW_E;
+     		NEXT_ROW_E+=BLOCK_SIZE;
+
+      pthread_mutex_unlock(&stillBusy);
+   
+   }
+
+   pthread_exit(NULL);
+
+}
+
+//Pthread version of block Transpose
+
+void* PthreadsblockTranspose(void* blockInfo){
+
+
+  struct blockThreadInfo *blockThread = (struct blockThreadInfo*) blockInfo;
+  
+	while(1){
+   
+	 	for(int i = blockThread->row; i < (blockThread->row+BLOCK_SIZE); i+=BLOCK_SIZE){
+      
+      			for(int j = 0; j < i; j+=BLOCK_SIZE)
+            		 {
+			     blockSwap(i,j, blockThread->arr);
+								 
+			 }
+		}
+
+         	
+	     //Finished?
+   	    if(NEXT_ROW_B >= SIZE - 1) break;
+  
+	    // Controlled Access
+
+  	     pthread_mutex_lock(&stillBusy);
+    
+   		 blockThread->row = NEXT_ROW_B;
+     		 NEXT_ROW_B+=BLOCK_SIZE;
+
+   	     pthread_mutex_unlock(&stillBusy);
+ 				
+		}
+
+   pthread_exit(NULL);
+}
+
+// Full Pthread Block Transposition
+
+void BlockTransposePthread(int twoD[SIZE][SIZE]){
+        
+   //For Element transposition
+
+   pthread_t eThreads[NUM_OF_THREADS];
+   struct blockThreadInfo eBlockThreads[NUM_OF_THREADS];
+   
+	 //Assuming size of the matrix will always be greater than 8
+    NEXT_ROW_E = NUM_OF_THREADS*BLOCK_SIZE;
+   // SIZE >> 8
+
+   for(int i = 0; i < NUM_OF_THREADS; i++){
+       
+        eBlockThreads[i].row = i*BLOCK_SIZE;
+        eBlockThreads[i].arr = twoD[0];
+        pthread_create(&eThreads[i], NULL, &PthreadsBlockElementTranspose, &eBlockThreads[i]);
+        
+
+    }
+
+   	for(int i =0; i <NUM_OF_THREADS; i++){
+
+              pthread_join(eThreads[i],NULL);
+    
+           }
+   
+
+	 //For Transposing the blocks
+
+   	pthread_t bThreads[NUM_OF_THREADS];
+   	struct blockThreadInfo blockThreads[NUM_OF_THREADS];
+   	NEXT_ROW_B = BLOCK_SIZE*NUM_OF_THREADS;
+
+   
+	 for(int i = 0; i < NUM_OF_THREADS; i++){
+       
+        	blockThreads[i].arr = twoD[0];
+       		blockThreads[i].row = i*BLOCK_SIZE;
+        	pthread_create(&bThreads[i], NULL, &PthreadsblockTranspose, &blockThreads[i]);
+
+    		}
+    
+      	for(int i =0; i <NUM_OF_THREADS; i++){
+
+              	pthread_join(bThreads[i],NULL);
+    
+           	}
+
+}
+
+
 
 
 //A timer function that accepts functions
@@ -343,6 +507,7 @@ int main()
 	timer(DiagonalTransposePthread,twoD,"Pthread Diagonal-Threading   ");
         //Block
         timer(BlockTransposeBasic,twoD,"Basic Block-Transposition    ");
+	timer(BlockTransposePthread,twoD,"Pthread Block-Transposition  ");
       
         printf("\n------------------------------------------------------------------");
 
@@ -356,3 +521,4 @@ int main()
 
 	return 0;
 }
+
